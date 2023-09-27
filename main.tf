4a64cf4234a5a96fcdac821d7500ab3d1ce1f136
Parth Solanki <parth.s@avdevs.com>
2023-09-27 16:09:36 +0530

    add push image using tf file

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
-resource "google_container_registry" "registry" {
-  project  = "klara-comm-nonprod"
-  location = "EU"
-}
-
+# resource "google_container_registry" "registry" {
+#   project  = "klara-comm-nonprod"
+#   location = "EU"
+# }
+#
 # resource "google_storage_bucket" "auto-expire" {
 #   name          = "terraform-test-static-bucket"
 #   project       = "klara-comm-nonprod"
@@ -11,3 +11,22 @@ resource "google_container_registry" "registry" {
 #
 #   public_access_prevention = "enforced"
 # }
+
+resource "null_resource" "push_to_gcr" {
+  triggers = {
+    # You can add triggers here if needed.
+  }
+
+  provisioner "local-exec" {
+    command = "docker"
+    working_dir = path.module
+
+    # Build and push the Docker image to GCR.
+    # Replace 'your-image' with the actual image name and tag.
+    arguments = [
+      "build",
+      "-t", "gcr.io/$PROJECT_ID/epost-hub:latest",
+      ".",
+    ]
+  }
+}
